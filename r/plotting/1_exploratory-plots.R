c3273450877455b06f79e3881bfb8af37a6dcad9
claudehspencer <claudespencer111@gmail.com>
2022-09-08 14:37:16 +0800

    starting depth cross section

--- r/plotting/1_exploratory-plots.R
+++ r/plotting/1_exploratory-plots.R
@@ -17,18 +17,18 @@ library(ggplot2)
 library(metR)
 library(stringr)
 library(patchwork)
-library(raster)
+library(terra)
 library(ggnewscale)
 library(GlobalArchive)
 library(tidyverse)
 library(viridis)
+library(marmap)
 
 working.dir <- getwd()
 setwd(working.dir)
 
 # Set CRS for transformations
-wgscrs <- CRS("+proj=longlat +datum=WGS84")
-sppcrs <- CRS("+proj=utm +zone=50 +south +datum=WGS84 +units=m +no_defs")     
+wgscrs <- "+proj=longlat +datum=WGS84" 
 
 # Get data and sort spatial boundaries
 aus    <- st_read("data/spatial/shapefiles/cstauscd_r.mif") %>%                 # Geodata 100k coastline available: https://data.gov.au/dataset/ds-ga-a05f7892-eae3-7506-e044-00144fdd4fa6/
@@ -55,12 +55,12 @@ cwatr  <- st_read("data/spatial/shapefiles/amb_coastal_waters_limit.shp")       
 # plot(damp_mp, add = T)
 
 bathy <- readRDS("data/spatial/rasters/GA_250m_bathy-trimmed.RDS")
-bathy <- rasterFromXYZ(bathy)
+bathy <- rast(bathy)
 crs(bathy) <- wgscrs
 
-slope <- terrain(bathy, opt='slope', unit='degrees')
-aspect <- terrain(bathy, opt='aspect', unit='degrees')
-hill <- hillShade(slope, aspect, angle = 70, azimuth = 0)
+slope <- terrain(bathy, v='slope', unit='degrees')
+aspect <- terrain(bathy, v='aspect', unit='degrees')
+hill <- shade(slope, aspect, angle = 70, direction = 0)
 
 # To dataframes for plotting
 hill <- as.data.frame(hill, xy = T, na.rm = T)
@@ -72,7 +72,7 @@ bathy <- as.data.frame(bathy, xy = T, na.rm = T)
 
 # build basic plot elements
 p1 <- ggplot() +
-  geom_tile(data = hill,aes(x = x, y = y, fill = layer), alpha = 1) +
+  geom_tile(data = hill,aes(x = x, y = y, fill = lyr1), alpha = 1) +
   scale_fill_gradient(low = "white", high = "black", guide = "none") +
   new_scale_fill() +
   geom_tile(data = bathy, aes(x = x, y = y, fill = Depth), alpha = 0.7) +
@@ -95,27 +95,19 @@ png(filename = "plots/exploratory-site-plot.png", height = 4, width = 10,
 p1
 dev.off()
 
-# p2 <- ggplot() + # Inset closer to the NPZ and HPZ
-#   geom_tile(data = hill,aes(x = x, y = y, fill = layer), alpha = 1) +
-#   scale_fill_gradient(low = "white", high = "black", guide = "none") +
-#   new_scale_fill() +
-#   geom_tile(data = bathy, aes(x = x, y = y, fill = Depth), alpha = 0.7) +
-#   scale_fill_viridis() +
-#   geom_contour(data = bathy, aes(x = x, y = y, z = Depth), binwidth = 10, 
-#                colour = "white", size = 0.1) +
-#   geom_sf(data = aus, fill = "seashell2", colour = "black", size = 0.1) +
-#   geom_sf(data = damp_mp, aes(color = ZoneName), fill = NA, size = 0.4) +
-#   scale_color_manual(values = c("Habitat Protection Zone" = "#fff8a3",
-#                                 "National Park Zone" = "#7bbc63",
-#                                 "Multiple Use Zone" = "#b9e6fb")) +
-#   geom_sf(data = cwatr, colour = "firebrick", alpha = 1, size = 0.4) +
-#   # coord_sf(xlim = c(min(bathy$x), max(bathy$x)), ylim = c(min(bathy$y), max(bathy$y))) +
-#   coord_sf(xlim = c(116.84, 117.15), ylim = c(-20.4, -20.3)) +
-#   labs(y = "Latitude", x = "Longitude")+
-#   theme_minimal()
-# p2
-# 
-# png(filename = "plots/exploratory-site-plot-w-inset.png", height = 8, width = 8,
-#     res = 300, units = "in")
-# p1 / p2 + plot_layout(guides = "collect")
-# dev.off()
+# Bathy cross section
+test <- marmap::as.bathy(bathy)
+test <- read.bathy("data/spatial/rasters/large/tile2c.txt")
+tes <- read.table("data/spatial/rasters/large/tile2c.txt")
+cbaths <- list.files("data/spatial/rasters/large", "*tile", full.names = TRUE)
+cbathy <- lapply(cbaths, function(x){read.table(file = x, header = TRUE, sep = ",")})
+cbathy <- do.call("rbind", lapply(cbathy, as.data.frame)) 
+
+test <- as.bathy(cbathy)
+plot.bathy(test)
+
+test1 <- get.transect(test, loc = T, dis = T)
+test1 <- get.transect(test, x1 = 112.1075, x2 = 113.1075, y1 = -14.1075, y2 = -13.1-75, distance = T)
+marmap::plotProfile(test1)
+
+head(test)
